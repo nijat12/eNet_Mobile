352dfcbe4ddfaa9048ed6891bb2e084e29411279
Nijat Zeynalov <nijat@miamidade.gov>
2015-03-13 00:54:58 -0400

    Skeleton of the app

--- www/js/app.js
+++ www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('eNetMobile', ['ionic', 'eNetMobile.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -26,8 +26,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
   .state('app', {
     url: "/app",
     abstract: true,
-    templateUrl: "templates/menu.html",
-    controller: 'AppCtrl'
+    templateUrl: "templates/menu.html"
   })
 
   .state('app.search', {
@@ -57,6 +56,12 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       }
     })
 
+    .state('login', {
+      url: "/login",
+      templateUrl: "templates/login.html",
+      controller: 'loginCtrl'
+    })
+
   .state('app.single', {
     url: "/playlists/:playlistId",
     views: {
@@ -67,5 +72,5 @@ angular.module('starter', ['ionic', 'starter.controllers'])
     }
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/login');
 });
